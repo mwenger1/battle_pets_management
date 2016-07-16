7ba09bb1e01b94a09ddb63c694b9da171fa33839
Michael Wenger <michaelwenger27@gmail.com>
2016-07-16 12:06:03 -0400

    Scaffolded trainer
    Removed fixture file as will be using [factory
    girl](https://github.com/thoughtbot/factory_girl)

---
+++ db/migrate/20160716160508_create_trainers.rb
@@ -0,0 +1,10 @@
+class CreateTrainers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :trainers do |t|
+      t.string :name
+      t.string :class
+
+      t.timestamps
+    end
+  end
+end
